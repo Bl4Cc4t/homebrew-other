605a39d844113fae98d02556c86efdb015d78314
Bl4Cc4t <kuron3ko@cock.li>
2018-06-04 17:30:07 +0200

    yay

---
+++ Formula/danmaku2ass.rb
@@ -0,0 +1,10 @@
+class Danmaku2ass < Formula
+  desc "Convert comments from Niconico/AcFun/bilibili to ASS format"
+  homepage "http://m13253.blogspot.com/2013/11/play-danmaku-offline-danmaku2ass.html"
+  head "https://github.com/m13253/danmaku2ass.git"
+
+  def install
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
