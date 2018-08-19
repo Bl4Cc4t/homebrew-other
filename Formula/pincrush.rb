9b079ec1bdc0236b582491a58d547f2bb2d98b38
Bl4Cc4t <kuron3ko@cock.li>
2018-08-19 15:20:31 +0200

    added pincrush

--- Formula/pincrush.rb
+++ Formula/pincrush.rb
@@ -7,6 +7,9 @@ class Pincrush < Formula
   head "https://github.com/DHowett/pincrush.git"
 
   def install
+    inreplace Dir["framework/makefiles/targets/Darwin/*.mk"], do |s|
+      s.gsub! "g++-4.2", "g++"
+    end
     system "make", "target=native"
     # system "make", "install"
     prefix.install Dir["*"]
