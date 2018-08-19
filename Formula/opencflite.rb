f57b51d992c2e1149816e45e70f1c638bb7bb619
Bl4Cc4t <kuron3ko@cock.li>
2018-08-19 22:12:24 +0200

    a

---
+++ Formula/opencflite.rb
@@ -0,0 +1,14 @@
+class OpenCFLite < Formula
+  desc "A cross platform toolkit for building applications on Mac OS X, Windows, and Linux."
+  homepage "https://sourceforge.net/projects/opencflite"
+  version "476.19.0"
+  url "https://sourceforge.net/projects/opencflite/files/opencflite/476.19.0/opencflite-476.19.0.tar.gz/download"
+  sha256 "3b593967e17de080491a5c18ee0938a6cce8f3f4a06ff62b042351e681fef549"
+  head "svn://svn.code.sf.net/p/opencflite/code/trunk"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"#, "PREFIX=#{prefix}"
+  end
+end
