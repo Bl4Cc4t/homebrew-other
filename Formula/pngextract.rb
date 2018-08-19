1990c35cf78e2f409bf6a83ec244cd3fc36309b4
Bl4Cc4t <kuron3ko@cock.li>
2018-08-20 00:01:59 +0200

    a

---
+++ Formula/pngextract.rb
@@ -0,0 +1,10 @@
+class Opencflite < Formula
+  desc "Extract embedded PNG images from (binary) files"
+  homepage "https://github.com/jomo/PNGExtract"
+  head "https://github.com/jomo/PNGExtract.git"
+
+  def install
+    prefix.install Dir["README.md", "LICENSE.txt"]
+    bin.install Dir["pngextract"]
+  end
+end
