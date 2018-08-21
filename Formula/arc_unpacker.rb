b3d582f6118b68800488f79792e4c9412eaa307e
Bl4Cc4t <kuron3ko@cock.li>
2018-08-21 19:42:03 +0200

    edited stuff

--- Formula/arc_unpacker.rb
+++ Formula/arc_unpacker.rb
@@ -15,11 +15,22 @@ class ArcUnpacker < Formula
   depends_on "webp" => :optional
   depends_on "cmake" => :build
 
+  # fix:
+  # -CMake Error at CMakeLists.txt:115 (else):
+  #    A duplicate ELSE command was found inside an IF block.
+  # -src/algo/call_stack_keeper.cc:33:15:
+  #    error: no matching conversion for functional-style cast from 'const char [24]' to 'err::GeneralError'
+  # -src/dec/jpeg/jpeg_image_decoder.cc:26:5:
+  #    error: no matching function for call to 'jpeg_read_header'
+  #    jpeg_read_header(&info, true);
   patch do
     url "https://raw.githubusercontent.com/Bl4Cc4t/homebrew-other/master/patches/arc_unpacker.diff"
     sha256 "6f87a6a5331dab13864bfc040fc35c801deffe281ed45e7f57935b0b18cab826"
   end
   def install
+    ENV.append "CPPFLAGS", "-I#{Formula["zlib"].opt_include}"
+    ENV.append "LDFLAGS", "-L#{Formula["zlib"].opt_lib}"
+
     (buildpath/"build").mkpath
     Dir.chdir("#{buildpath}/build")
     system "cmake", "-DCMAKE_BUILD_TYPE=release", ".."
