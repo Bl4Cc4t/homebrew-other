7a8ec7f223def82c3589f37cde56a9634f33f81b
Bl4Cc4t <kuron3ko@cock.li>
2018-08-23 21:45:16 +0200

    edited stuff

--- Formula/wxmedit.rb
+++ Formula/wxmedit.rb
@@ -1,9 +1,9 @@
 class Wxmedit < Formula
   desc "wxMEdit, a cross-platform Text/Hex Editor, an improved version of MadEdit"
   homepage "http://wxmedit.github.io/"
-  version "0.11"
-  url "https://github.com/wxMEdit/wxMEdit/archive/3.1_FIX1.tar.gz"
-  sha256 "51dd81ddd44c83017d2e54f8b0a11a50e56075e8edc01d738865478fdddd8017"
+  # version "0.11"
+  # url "https://github.com/wxMEdit/wxMEdit/archive/3.1_FIX1.tar.gz"
+  # sha256 "51dd81ddd44c83017d2e54f8b0a11a50e56075e8edc01d738865478fdddd8017"
   head "https://github.com/wxMEdit/wxMEdit.git"
 
   depends_on "autoconf" => :build
@@ -16,10 +16,21 @@ class Wxmedit < Formula
   depends_on "icu4c"
   depends_on "wxwidgets"
 
+  # option "with-bundle", "Enable compilation of the .app bundle."
+
   def install
+    inreplace Dir["src/mad_utils.cpp", "src/wxm/utils.cpp"] do |s|
+      s.gsub! "wx/mac/private.h", "wx/msw/private.h"
+    end
+
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+
+    # if build.with? "bundle"
+    #   system "python", "TOOLS/osxbundle.py", "build/mpv"
+    #   prefix.install "build/mpv.app"
+    # end
   end
 end
