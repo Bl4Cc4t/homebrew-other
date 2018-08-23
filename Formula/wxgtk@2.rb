f0e17cdfea7c8415cd31e937eff8ccbcb15a2088
Bl4Cc4t <kuron3ko@cock.li>
2018-08-23 23:58:20 +0200

    edited stuff

--- Formula/wxgtk@2.rb
+++ Formula/wxgtk@2.rb
@@ -5,7 +5,7 @@ class WxgtkAT2 < Formula
   url "http://downloads.sourceforge.net/project/wxwindows/2.8.12/wxGTK-2.8.12.tar.gz"
   sha256 '13cf89f2c29bcb90bb56a31ac1af10f23003d3d43c3e4b24991518f5dc4e5abe'
 
-  # depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on :x11 # if your formula requires any X11/XQuartz components
   depends_on "pkg-config" => :build
   depends_on 'gtk+'
   depends_on "libpng"
@@ -38,7 +38,7 @@ class WxgtkAT2 < Formula
       "--without-macosx-sdk"
     ]
     ENV.append "CFLAGS", '-arch x86_64' # if MacOS.prefer_64_bit?
-    ENV.append 'LDFLAGS', '-lGL -lGLU'
+    ENV.append 'LDFLAGS', '-lX11 -lGL -lGLU'
     # ENV.append "CFLAGS", "-fPIC"
     system "./configure", *args
 
