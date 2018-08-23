b534cc431df46d67b77b6a04de9f228428b7e273
Bl4Cc4t <kuron3ko@cock.li>
2018-08-23 23:03:02 +0200

    edited stuff

--- Formula/wxgtk@2.rb
+++ Formula/wxgtk@2.rb
@@ -6,7 +6,7 @@ class WxgtkAT2 < Formula
   url "http://downloads.sourceforge.net/project/wxwindows/2.8.12/wxGTK-2.8.12.tar.gz"
   sha256 '13cf89f2c29bcb90bb56a31ac1af10f23003d3d43c3e4b24991518f5dc4e5abe'
 
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  # depends_on :x11 # if your formula requires any X11/XQuartz components
   depends_on 'gtk+'
 
   def install
