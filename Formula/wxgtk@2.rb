34a154b4d62c2fb5d2453f2bd9a4a4f007fef086
Bl4Cc4t <kuron3ko@cock.li>
2018-08-24 12:37:25 +0200

    edited stuff

--- Formula/wxgtk@2.rb
+++ Formula/wxgtk@2.rb
@@ -56,7 +56,7 @@ class WxgtkAT2 < Formula
     # ENV.append "CFLAGS", "-arch", "x86_64" # if MacOS.prefer_64_bit?
     ENV.append "LDFLAGS"#, "-lGL", "-lGLU", "-lX11"
     ENV.append "LDFLAGS", "-L/opt/X11/lib/"
-    ENV.append "CPPFLAGS", "-L/opt/X11/include"
+    ENV.append "CPPFLAGS", "-I/opt/X11/include"
     system "./configure", *args
 
     system "make install"
