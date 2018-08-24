644b9fb6d5f604faac779a6b175909c22871aa01
Bl4Cc4t <kuron3ko@cock.li>
2018-08-24 12:37:14 +0200

    edited stuff

--- Formula/wxgtk@2.rb
+++ Formula/wxgtk@2.rb
@@ -54,9 +54,9 @@ class WxgtkAT2 < Formula
       "--without-sdl",
     ]
     # ENV.append "CFLAGS", "-arch", "x86_64" # if MacOS.prefer_64_bit?
-    ENV.append "LDFLAGS", "-framework Carbon -framework OpenGL -framework GLUT"#, "-lGL", "-lGLU", "-lX11"
-    # ENV.append "LDFLAGS", "-L/opt/X11/lib"
-    # ENV.append "CPPFLAGS", "-L/opt/X11/include"
+    ENV.append "LDFLAGS"#, "-lGL", "-lGLU", "-lX11"
+    ENV.append "LDFLAGS", "-L/opt/X11/lib/"
+    ENV.append "CPPFLAGS", "-L/opt/X11/include"
     system "./configure", *args
 
     system "make install"
