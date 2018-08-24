db2ab3242e93d843ec643c02ad6bbd222a34ca54
Bl4Cc4t <kuron3ko@cock.li>
2018-08-24 12:39:03 +0200

    edited stuff

--- Formula/wxgtk@2.rb
+++ Formula/wxgtk@2.rb
@@ -54,7 +54,7 @@ class WxgtkAT2 < Formula
       "--without-sdl",
     ]
     # ENV.append "CFLAGS", "-arch", "x86_64" # if MacOS.prefer_64_bit?
-    ENV.append "LDFLAGS"#, "-lGL", "-lGLU", "-lX11"
+    # ENV.append "LDFLAGS", "-lGL", "-lGLU", "-lX11"
     ENV.append "LDFLAGS", "-L/opt/X11/lib/"
     ENV.append "CPPFLAGS", "-I/opt/X11/include"
     system "./configure", *args
