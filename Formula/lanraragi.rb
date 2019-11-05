d5b1fa97093472c08df00ee65eca79d339c9fa29
Bl4Cc4t <kuron3ko@cock.li>
2019-11-05 22:08:18 +0100

    remove unnecessary stuff

--- Formula/lanraragi.rb
+++ Formula/lanraragi.rb
@@ -3,19 +3,18 @@ require "language/node"
 class Lanraragi < Formula
   desc "Web application for archival and reading of manga/doujinshi. Lightweight and Docker-ready for NAS/servers."
   homepage "https://github.com/Difegue/LANraragi"
-  version "0.6.6"
   url "https://github.com/Difegue/LANraragi/archive/v.0.6.6.tar.gz"
   sha256 "bc4e8db0bc4570390f5e92327ecb33caabea98696b3708f270aa06775935716b"
   head "https://github.com/Difegue/LANraragi.git"
 
   depends_on "pkg-config" => :build
   depends_on "cpanminus"
+  depends_on "jpeg"
   depends_on "libarchive"
-  depends_on "libjpeg"
   depends_on "libpng"
   depends_on "node"
   depends_on "openssl"
-  depends_on "perl" => "5.20.2"
+  depends_on "perl"
   depends_on "redis"
 
   def install
