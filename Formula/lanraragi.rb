741fc5eeed8f88e1f6e53d0512a75100349d8105
Bl4Cc4t <kuron3ko@cock.li>
2019-11-07 21:25:45 +0100

    added test block, switched back to own fork for the time being

--- Formula/lanraragi.rb
+++ Formula/lanraragi.rb
@@ -5,7 +5,7 @@ class Lanraragi < Formula
   homepage "https://github.com/Difegue/LANraragi"
   url "https://github.com/Difegue/LANraragi/archive/v.0.6.6.tar.gz"
   sha256 "bc4e8db0bc4570390f5e92327ecb33caabea98696b3708f270aa06775935716b"
-  head "https://github.com/Difegue/LANraragi.git"
+  head "https://github.com/Bl4Cc4t/LANraragi.git", :branch => "homebrew-test"
 
   depends_on "pkg-config" => :build
   depends_on "cpanminus"
@@ -33,5 +33,15 @@ class Lanraragi < Formula
     prefix.install "log"
     prefix.install "tools/homebrew-start/redis.conf"
     bin.install "tools/homebrew-start/lanraragi"
+    prefix.install "tests"
+  end
+
+  test do
+    ENV["PERL5LIB"] = libexec/"lib/perl5"
+    ENV["LRR_LOG_DIRECTORY"] = testpath/"log"
+
+    system "redis-server", "--daemonize", "yes"
+    system "npm", "--prefix", prefix, "test"
+    system "redis-cli", "shutdown"
   end
 end
