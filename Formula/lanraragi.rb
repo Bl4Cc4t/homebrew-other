71928c9c187d51805d9386585636ba964afa620e
Bl4Cc4t <kuron3ko@cock.li>
2020-03-16 00:00:45 +0100

    add imagemagick related stuff

--- Formula/lanraragi.rb
+++ Formula/lanraragi.rb
@@ -5,7 +5,7 @@ class Lanraragi < Formula
   homepage "https://github.com/Difegue/LANraragi"
   url "https://github.com/Difegue/LANraragi/archive/v.0.6.7.tar.gz"
   sha256 "3b47fa5eae1e1bf2c25a328c38ab8f3c5807801dce53ff27968f7fab3f38dc43"
-  head "https://github.com/Difegue/LANraragi.git", :branch => "master"
+  head "https://github.com/Difegue/LANraragi.git", :branch => "dev"
 
   depends_on "pkg-config" => :build
   depends_on "cpanminus"
@@ -17,10 +17,27 @@ class Lanraragi < Formula
   depends_on "openssl"
   depends_on "perl"
   depends_on "redis"
+  depends_on "imagemagick@6"
+
+
+  resource "Image::Magick" do
+    url "https://cpan.metacpan.org/authors/id/J/JC/JCRISTY/PerlMagick-6.89-1.tar.gz"
+    sha256 "c8f81869a4f007be63e67fddf724b23256f6209f16aa95e14d0eaef283772a59"
+  end
 
   def install
     ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
     ENV.prepend_path "PERL5LIB", libexec/"lib"
+
+    resource("Image::Magick").stage do
+      inreplace "Makefile.PL" do |s|
+        s.gsub! "include/ImageMagick-6", "opt/imagemagick@6/include/ImageMagick-6"
+      end
+      
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
 
     system "npm", "install", *Language::Node.local_npm_install_args
     system "perl", "./tools/install.pl", "install-full"
